39e64b9eac09a692c44f64443819553849198ada
Andrew Morcom <jubulani@fastmail.fm>
2018-12-31 15:34:35 +0200

    Add workaround for integration problem with electron

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -3,8 +3,14 @@ extern crate neon;
 
 use neon::prelude::*;
 
+// https://users.rust-lang.org/t/neon-electron-undefined-symbol-cxa-pure-virtual/21223
+#[no_mangle]
+pub extern fn __cxa_pure_virtual() {
+    loop{};
+}
+
 fn thread_count(mut cx: FunctionContext) -> JsResult<JsNumber> {
-	Ok(cx.number(num_cpus::get() as f64))
+    Ok(cx.number(num_cpus::get() as f64))
 }
 
 register_module!(mut cx, {
