4ecd80f5a17a7b136123128c51602ed53f4b5777
Andrew Morcom <jubulani@fastmail.fm>
2019-01-03 00:24:10 +0200

    Actually read some file data

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -1,9 +1,39 @@
 #[macro_use]
 extern crate neon;
 
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::Read;
+
 use neon::prelude::*;
 
-static mut COUNTER: usize = 0;
+static mut PROGRAM_DATA: Option<ProgramData> = None;
+
+struct ProgramData {
+    file_data: HashMap<usize, FileData>,
+}
+
+impl ProgramData {
+    fn new() -> ProgramData {
+        ProgramData { file_data: HashMap::new() }
+    }
+
+    fn get_next_handle(&self) -> usize {
+        self.file_data.len()
+    }
+
+    fn set_file_data(&mut self, handle: usize, file_data: FileData) {
+        if self.file_data.contains_key(&handle) {
+            panic!(format!("Key already exists: {}", handle));
+        }
+
+        self.file_data.insert(handle, file_data);
+    }
+}
+
+struct FileData {
+    buffer: Vec<u8>
+}
 
 // Get rid of 'undefined function __cxa_pure_virtual' error.
 // See: https://users.rust-lang.org/t/neon-electron-undefined-symbol-cxa-pure-virtual/21223
@@ -12,18 +42,37 @@ pub extern fn __cxa_pure_virtual() {
     loop{};
 }
 
+fn get_program_data() -> &'static mut ProgramData {
+    let ret;
+    unsafe {
+        ret = PROGRAM_DATA.get_or_insert_with(ProgramData::new)
+    }
+    ret
+}
+
+fn read_file(filename: &str) -> FileData {
+    let mut f = File::open(filename).expect(&format!("File not found: {}", filename));
+    let mut buffer = Vec::new();
+
+    f.read_to_end(&mut buffer).expect(&format!("Error reading file: {}", filename));
+
+    FileData { buffer }
+}
+
 fn open_file(mut cx: FunctionContext) -> JsResult<JsNumber> {
+
     // First argument is filename as a string
     let filename = cx.argument::<JsString>(0)?.value();
 
-    //let _: JsResult<JsError> = cx.throw_error("File not found");
+    let program_data = get_program_data();
 
-    let ret = format!("Opening file: {}", filename);
-    println!("Open file: {}", filename);
-    unsafe {
-        COUNTER += 1;
-        Ok(cx.number(COUNTER as f64)) 
-    }
+    let handle = program_data.get_next_handle();
+
+    let file_data = read_file(&filename);
+
+    program_data.set_file_data(handle, file_data);
+
+    Ok(cx.number(handle as f64))
 }
 
 register_module!(mut cx, {
