c578077e46b1aca3433a2e63054a2ec1ab77549e
Andrew Morcom <jubulani@fastmail.fm>
2019-01-02 21:53:32 +0200

    Test global variables

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -3,20 +3,27 @@ extern crate neon;
 
 use neon::prelude::*;
 
-// https://users.rust-lang.org/t/neon-electron-undefined-symbol-cxa-pure-virtual/21223
+static mut COUNTER: usize = 0;
+
+// Get rid of 'undefined function __cxa_pure_virtual' error.
+// See: https://users.rust-lang.org/t/neon-electron-undefined-symbol-cxa-pure-virtual/21223
 #[no_mangle]
 pub extern fn __cxa_pure_virtual() {
     loop{};
 }
 
-fn open_file(mut cx: FunctionContext) -> JsResult<JsString> {
+fn open_file(mut cx: FunctionContext) -> JsResult<JsNumber> {
     // First argument is filename as a string
     let filename = cx.argument::<JsString>(0)?.value();
 
     let _: JsResult<JsError> = cx.throw_error("File not found");
 
     let ret = format!("Opening file: {}", filename);
-    Ok(cx.string(ret)) 
+    println!("Open file: {}", filename);
+    unsafe {
+        COUNTER += 1;
+        Ok(cx.number(COUNTER as f64)) 
+    }
 }
 
 register_module!(mut cx, {
